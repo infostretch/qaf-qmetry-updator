4847a638b41bc022d067f9e37e8c5f552ba47b3c
anjalibangoria <bangoria12anjali@gmail.com>
2016-09-07 17:01:56 +0530

    get info user as json element

--- src/com/qmetry/qaf/automation/integration/qmetry/qmetry6/QMetryRestWebservice.java
+++ src/com/qmetry/qaf/automation/integration/qmetry/qmetry6/QMetryRestWebservice.java
@@ -261,7 +261,7 @@ public class QMetryRestWebservice {
 	 *         for fail it may be 1001 etc.
 	 */
 	public String getRunStatusID(TestCaseRunResult status) {
-		JsonObject jsonObject = new Gson().fromJson(userInfo, JsonObject.class);
+		JsonObject jsonObject = new Gson().fromJson(userInfo, JsonElement.class).getAsJsonObject();
 		JsonArray array = jsonObject.get("allstatus").getAsJsonArray();
 
 		Map<String, String> statusMap = new HashMap<String, String>();
@@ -404,6 +404,7 @@ public class QMetryRestWebservice {
 	}
 
 	/**
+	 * 
 	 * @param baseURL
 	 * @param token
 	 *            - token generate using username and password
